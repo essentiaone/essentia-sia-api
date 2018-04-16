5bf802425b8f0f4a402647f43665fa5876635ff9
a_ruban <alexander.ruban98@gmail.com>
2018-04-16 11:41:05 +0300

    Temp

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'requests<=2.18.4',
+        'requests',
     ],
     classifiers=[
         'Development Status :: 3 - Alpha',
